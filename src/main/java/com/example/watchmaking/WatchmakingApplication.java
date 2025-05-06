9815c129500c4bc93ed75ab8abfa94b5ba111729
Vinicius <viniciusbgoulart@hotmail.com>
2025-05-06 15:57:38 -0300

    Started watchmarket project

---
+++ src/main/java/com/example/watchmaking/WatchmakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.watchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchmakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchmakingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/watchmaking/WatchmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchmakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
